143545985e9ea3bf2f177f7e62b1e3db2fd31a90
Mela1805 <melania.borcan@student.upt.ro>
2025-01-12 18:11:23 +0200

    Clasa Organizator moștenește clasa User, ceea ce înseamnă că preia toate proprietățile și metodele acesteia. Constructorul clasei Organizator primește cinci parametri (id, firstName, lastName, email, password) și îi transmite direct constructorului clasei de bază prin base(...). Momentan, clasa nu adaugă funcționalitate proprie, dar poate fi extinsă ulterior pentru a include elemente specifice unui organizator.

---
+++ ConsoleApp1/Organizator.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApp1;
+
+public class Organizator : User
+{
+    public Organizator(int id, string firstName, string lastName, string email, string password)
+        : base(id, firstName, lastName, email, password) { }
+}
