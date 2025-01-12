8e7eca299fc46616c33d57d3907d5853981002ee
Miliana23 <miliana.misici@student.upt.ro>
2025-01-12 17:49:48 +0200

    Clasa numită Client, care extinde clasa abstractă User din același namespace, ConsoleApp1, moștenește toate proprietățile și funcționalitățile clasei User, cum ar fi Id, FirstName, LastName, Email și Password.
    Constructorul clasei Client primește valorile necesare pentru aceste proprietăți și le pasează constructorului clasei de bază User folosind cuvântul cheie base. Astfel, clasa Client poate fi utilizată pentru a crea instanțe concrete de utilizatori care sunt clienți, fără a adăuga încă funcționalități suplimentare.
    
    Această clasă este un exemplu de cum poate fi extinsă clasa abstractă User pentru a reprezenta tipuri specifice de utilizatori.

---
+++ ConsoleApp1/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1;
+
+public class Client : User
+
+{
+    public Client(int id, string firstName, string lastName, string email, string password)
+        : base(id, firstName, lastName, email,password){}
+
+    
+}
