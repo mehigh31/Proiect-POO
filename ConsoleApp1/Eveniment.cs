53fe333126c756e533def4bdcdb69af49e558847
Miliana23 <miliana.misici@student.upt.ro>
2025-01-12 19:04:11 +0200

    Clasa Eveniment din namespace-ul ConsoleApp1 definește o entitate pentru un eveniment. Are proprietăți precum Id, Name, Description, Capacity, Date și o listă de participanți (Participants). Constructorul permite inițializarea acestor proprietăți atunci când se creează un obiect.

---
+++ ConsoleApp1/Eveniment.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1;
+
+public class Eveniment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Capacity { get; set; }
+    public DateTime Date { get; set; }
+    public List<Client> Participants { get; set; } = new List<Client>();
+
+    public Eveniment(int id, string name, string description, int capacity, DateTime date)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Capacity = capacity;
+        Date = date;
+    }
+}
