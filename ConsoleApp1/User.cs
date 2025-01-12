c91415cb8e29ddfa6a4a6e15c72fbeb5d6045d21
Miliana23 <miliana.misici@student.upt.ro>
2025-01-12 17:22:53 +0200

    Programul definește o clasă abstractă numită User în cadrul unui proiect organizat în namespace-ul ConsoleApp1. Clasa User este un model general pentru utilizatori și conține cinci proprietăți: Id, FirstName, LastName, Email și Password. Acestea sunt inițializate printr-un constructor protejat, care poate fi apelat doar de clasele care moștenesc User.
    Fiind abstractă, clasa User nu poate fi instanțiată direct, dar oferă o bază comună pentru alte tipuri de utilizatori, cum ar fi clienți sau organizatori. Scopul ei este să simplifice gestionarea utilizatorilor într-o aplicație, centralizând caracteristicile comune.

---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1;
+
+public abstract class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    protected User(int id, string firstName, string lastName, string email, string password)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+
+}
